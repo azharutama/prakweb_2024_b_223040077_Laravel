984623eb0dc5c34e3a701989348ee8dc43e92e3a
azharutama <azharutama837@gmail.com>
2024-10-23 19:11:39 +0700

    VIDIO 3 Laravel 11 - Struktur folder

---
+++ resources/views/about.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>about</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  
+  <a href="/">home</a>
+  <a href="/about">about</a>
+  <h1>about page</h1>
+<h3>owner: {{ $nama }}</h3>
+  <img src="img/1.png" alt="">
+</body>
+</html>
