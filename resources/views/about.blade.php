c8e7880640eb754604d66ee079716bfab6c4bbc5
azharutama <azharutama837@gmail.com>
2024-10-23 23:00:30 +0700

    VIDIO 5 Laravel 11 - Blade component

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,21 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>about</title>
-  <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
+<x-layout>
   
-  <a href="/">home</a>
-  <a href="/about">about</a>
-  <a href="/blog">blog</a>
-  <a href="/contact">blog</a>
-
-  <h1>about page</h1>
-<h3>owner: {{ $nama }}</h3>
-  <img src="img/1.png" alt="">
-</body>
-</html>
+  <x-slot:title>{{ $title }}</x-slot:title>
+  <h3 class="text-xl">ini adalah about page</h3>
+  <p>Nama saya {{ $nama }}</p>
+</x-layout>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,22 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>contact</title>
-</head>
-<body>
-  <a href="/">home</a>
-  <a href="/about">about</a>
-  <a href="/blog">blog</a>
-  <a href="/contact">blog</a>
-
-  <h1>contact page</h1>
-  <ul>
-    <li><a href="https://instagram.com">Instagram</a></li>
-    <li><a href="https://facebook.com">facebook</a></li>
-    <li><a href="https://x.com">Twiter</a></li>
-  </ul>
-</body>
-</html>
+<x-layout>
+  
+  <x-slot:title>{{ $title }}</x-slot:title>
+  <h3 class="text-xl">ini adalah contact page</h3>
+</x-layout>
