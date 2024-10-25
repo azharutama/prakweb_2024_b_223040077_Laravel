8b9623589afde18c1ae7de34c928d5665d38cac4
azharutama <azharutama837@gmail.com>
2024-10-25 20:31:02 +0700

    VIDIO 12 Laravel 11 - Post category

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,11 +6,16 @@
   @foreach ($posts as $post)
  
   <article class="py-8 px-8 max-w-screen-md border-b border-gray-300">
-   <a href="/posts/{{ $post['slug'] }}"> <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
-   </a>
-    <div class="text-base text-grey-500 ">
-      <a href="/authors/{{ $post->author->id }}" class="hover:underline"> {{ $post->author->name }}</a> | {{ $post->created_at->diffForHumans()}}
-    </div>
+
+   <a href="/posts/{{ $post['slug'] }}"> <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2></a>
+
+   <div>
+    By
+      <a href="/authors/{{ $post->author->username}}" class="text-base text-gray-500 hover:underline"> {{ $post->author->name }}</a> 
+    in
+      <a href="/categories/{{ $post->category->slug }}" class="text-base text-gray-500  hover:underline"> {{ $post->category->name }}</a> |
+       {{ $post->created_at->diffForHumans()}}
+  </div>
 
     <p class="my-4 font-light">{{ Str::limit($post['body'], 50) }}</p>
 
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Post;
 use App\Models\User;
+use App\Models\Category;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
@@ -30,8 +31,14 @@ Route::get('/posts/{post:slug}', function (Post $post) {
     return view('post', ['title' => 'Single post', 'post' => $post]);
 });
 
-Route::get('/authors/{user}', function (User $user) {
+Route::get('/authors/{user:username}', function (User $user) {
 
     // $post = Post::find($slug);
-    return view('posts', ['title' => 'Article by' . $user->name, 'posts' => $user->posts]);
+    return view('posts', ['title' => count($user->posts) . ' Article by ' . $user->name, 'posts' => $user->posts]);
+});
+
+Route::get('/categories/{category:slug}', function (Category $category) {
+
+    // $post = Post::find($slug);
+    return view('posts', ['title' =>  ' Article in Category : ' . $category->name, 'posts' => $category->posts]);
 });
