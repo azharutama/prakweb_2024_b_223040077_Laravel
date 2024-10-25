affa9f69b8ef7e89e00c8a9c40d08d4aba757127
azharutama <azharutama837@gmail.com>
2024-10-25 22:14:23 +0700

    VIDIO 13 Laravel 11 - Database seeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Category::created([
+
+        //     'name' => 'Web Design',
+        //     'slug' => 'web-Desain'
+        // ]);
+
+        // Category::created([
+
+        //     'name' => 'Intelegensia Buatan',
+        //     'slug' => 'Intelegensia-Buatan'
+        // ]);
+
+        // Category::created([
+
+        //     'name' => 'UI UX',
+        //     'slug' => 'UI-UX'
+        // ]);
+        Category::factory(3)->create();
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+
+    public function run(): void
+    {
+        User::factory(5)->create();
+    }
+}
