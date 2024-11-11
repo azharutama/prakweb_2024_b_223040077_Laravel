1c156f4d3dc2975626489948e100eb234a05899c
azharutama <azharutama837@gmail.com>
2024-11-11 21:21:35 +0700

    VIDIO 16 Laravel 8 - User Login & middleware

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -13,5 +15,32 @@ class LoginController extends Controller
 
 
         ]);
+    }
+
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('dashboard');
+        }
+
+        return back()->with('loginError', 'login failed!');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+
+        request()->session()->invalidate();
+
+        request()->session()->regenerateToken();
+
+        return redirect('/');
     }
 }
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -16,15 +16,42 @@
             </div>
           </div>
         </div>
-        <div class="hidden md:block">
-          <div class="ml-4 flex items-center md:ml-6">
-            <!-- Profile dropdown -->
-            <div class="relative ml-3">
-              <div>
+
+        @auth
+          <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-white bg-black-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Hello, {{ auth()->user()->name }}<svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+            </svg>
+            </button>
+            
+            <!-- Dropdown menu -->
+            <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+                  <li>
+                    <a href="/dashboard" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                  </li>
+                  <li>
+                    <form action="/logout" method="post">
+                      @csrf
+                      <button type="submit" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"> Logout</button>
+                    </form>
+                    
+                  </li>
                 
-              </div>
-              <x-nav-link href="/login" :active="request()->is('login')"> <Button>Sign In</Button></x-nav-link>
+                </ul>
             </div>
+            
+          @else
+          <div class="hidden md:block">
+            <div class="ml-4 flex items-center md:ml-6">
+             
+              <div class="relative ml-3">
+                <div>
+                  
+                </div>
+                <x-nav-link href="/login" :active="request()->is('login')"> <Button>Sign In</Button></x-nav-link>
+              </div>
+        @endauth
+        
          
             
 
