99401357e0b4dd424ed311c946fb852a443f690a
azharutama <azharutama837@gmail.com>
2024-11-11 16:36:11 +0700

    VIDIO 15 Laravel 8 - User registration

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+
+use function Laravel\Prompts\password;
+use Illuminate\Routing\Controller;
 
 class RegisterController extends Controller
 {
@@ -13,5 +19,26 @@ class RegisterController extends Controller
 
 
         ]);
+    }
+
+    public function store(Request $request)
+    {
+
+
+        $data = $request->validate([
+            'name' => 'required|max:255',
+            'username' => 'required|min:5|max:20|unique:users',
+            'email' => 'required|email:dns|',
+            'password' => 'required|min:6|max:100',
+        ]);
+        $data['password'] = Hash::make($data['password']);
+
+
+
+        User::create($data);
+
+
+
+        return redirect('/login')->with('success', 'Registration successful!');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -47,4 +47,9 @@ Route::get('/categories/{category:slug}', function (Category $category) {
 
 
 Route::get('/login', [LoginController::class, 'index']);
+
 Route::get('/register', [RegisterController::class, 'index']);
+Route::post('/register', [RegisterController::class, 'store']);
+Route::middleware(['web'])->group(function () {
+    Route::post('/register', [RegisterController::class, 'store']);
+});
